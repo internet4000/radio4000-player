a2e278d1e077844be0657d22e46b87137835f0cd
Oskar Rough <oskar@rough.dk>
2017-03-16 23:22:07 +0100

    Add basic controls for the Youtube player

---
+++ src/YoutubeControls.vue
@@ -0,0 +1,38 @@
+<!--
+https://developers.google.com/youtube/iframe_api_reference#Playback_controls
+-->
+
+<template>
+	<menu id="Controls">
+		<button @click="play">Play</button>
+		<button @click="pause">Pause</button>
+		<button @click="mute">Mute</button>
+		<button @click="unMute">Unmute</button>
+	</menu>
+</template>
+
+<script>
+export default {
+	name: 'youtube-controls',
+	props: ['player'],
+	methods: {
+		play() { this.player.playVideo() },
+		pause() { this.player.pauseVideo() },
+		mute() { this.player.mute() },
+		unMute() { this.player.unMute() },
+	}
+}
+</script>
+
+<style scoped>
+	#Controls {
+		margin: 0;
+		padding: 0;
+		display: flex;
+	}
+	button {
+		flex: 1;
+		min-height: 3em;
+	}
+</style>
+
--- src/YoutubePlayer.vue
+++ src/YoutubePlayer.vue
@@ -1,15 +1,25 @@
 <template>
 	<div id="YoutubePlayer">
-		<div id="player"></div>
+		<youtube-controls :player="player" />
+
+		<div class="Ratio">
+			<div id="player"></div>
+		</div>
 	</div>
 </template>
 
 <script>
 import YouTubePlayer from 'youtube-player'
+import YoutubeControls from './YoutubeControls.vue'
 
 export default {
   name: 'youtube-player',
 	props: ['video-id'],
+	data() {
+		return {
+			player: {}
+		}
+	},
 	watch: {
 		videoId(id) {
 			this.player.loadVideoById(id)
@@ -17,12 +27,13 @@ export default {
 	},
 	mounted() {
     this.player = YouTubePlayer('player');
-	}
+	},
+	components: {YoutubeControls}
 }
 </script>
 
 <style scoped>
-#YoutubePlayer {
+.Ratio {
 	overflow: hidden;
 	position: relative;
 	height: 0;
@@ -31,7 +42,7 @@ export default {
 </style>
 
 <style>
-#YoutubePlayer iframe {
+.Ratio iframe {
 	position: absolute;
 	top: 0;
 	left: 0;
