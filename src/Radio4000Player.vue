dc05a1fedc80280771395481d8721b9d671abd21
Oskar Rough <oskar@rough.dk>
2017-04-19 21:03:54 +0200

    Make it a custom element

---
+++ src/Radio4000Player.vue
@@ -0,0 +1,252 @@
+<template>
+	<div>
+		<!--
+		<p>
+			<label>
+				<input type="checkbox" v-model="skins.dark">
+				Dark
+			</label><br>
+			<label>
+				<input type="checkbox" v-model="skins.mini">
+				Mini
+			</label>
+		</p>
+		-->
+		<div class="Radio4000Player" :class="{dark: skins.dark, mini: skins.mini}">
+			<header>
+				<channel-header
+					:channel="channel"
+					:image="image"
+					:track="track"></channel-header>
+			</header>
+			<aside>
+				<youtube-player
+					:video-id="track.ytid"
+					:volume="volume"
+					:autoplay="autoplay"
+					:playing="playing"
+					@error="onPlayerError"
+					@ready="onPlayerReady"
+					@playing="onPlayerPlaying"
+					@ended="onPlayerEnded"
+					></youtube-player>
+			</aside>
+			<main>
+				<track-list
+				v-if="tracks"
+				:tracks="tracks"
+				@select="selectTrack"></track-list>
+		</main>
+		<footer>
+			<player-controls
+				v-if="playerReady"
+				:player="player"
+				:playing="playing"
+				@play="play"
+				@pause="pause"
+				@next="next"
+				></player-controls>
+		</footer>
+	</div>
+</div>
+	</template>
+
+	<script>
+import Vue from 'vue'
+import ChannelHeader from './ChannelHeader.vue'
+import TrackList from './TrackList.vue'
+import YoutubePlayer from './YoutubePlayer.vue'
+import PlayerControls from './PlayerControls.vue'
+import store from './store'
+
+export default {
+	name: 'radio4000-player',
+	components: {
+		ChannelHeader,
+		TrackList,
+		YoutubePlayer,
+		PlayerControls
+	},
+	props: {
+		slug: String,
+		volume: Number
+	},
+	data () {
+		return {
+			playerReady: false,
+			autoplay: false,
+			loop: false,
+			playing: false,
+			skins: {
+				mini: false,
+				dark: false
+			},
+			channel: {
+				title: 'Loading Radio4000...'
+			},
+			tracks: [],
+			track: {},
+			image: ''
+		}
+	},
+	created() {
+		if (this.slug) {
+			this.model(this.slug).then(data => {
+				this.cueTrack(data.tracks[0])
+			})
+		}
+	},
+	watch: {
+		slug: function (a) {
+			this.model(this.slug).then(data => {
+				this.cueTrack(data.tracks[0])
+			})
+		}
+	},
+	methods: {
+		selectTrack(track) {
+			this.autoplay = true
+			this.cueTrack(track)
+		},
+		cueTrack(track) {
+			this.tracks.forEach(t => {t.active = false})
+			track.active = true
+			this.track = track
+		},
+		// runs once on load when yt-iframe is ready
+		onPlayerReady(player) {
+			this.playerReady = true
+		},
+		onPlayerError(event) {
+			console.log({youtubeError: event.data})
+			this.next()
+		},
+		onPlayerPlaying(event) {
+			this.playing = true
+		},
+		onPlayerEnded(event) {
+			console.log('onPlayerEnded')
+			this.playing = false
+			this.next()
+		},
+		play() {
+			this.playing = true
+		},
+		pause() {
+			this.playing = false
+		},
+		next() {
+			const index = this.tracks.indexOf(this.track)
+			let track = this.tracks[index + 1]
+			if (!track && this.loop) {
+				track = this.tracks[0]
+			}
+			if (!track) {
+				return
+			}
+			this.autoplay = true
+			this.cueTrack(track)
+		},
+		model(slug) {
+			return store.findChannelBySlug(slug).then(channel => {
+				this.channel = channel
+				const imageId = Object.keys(channel.images)[0]
+				if (imageId) {
+					store.findImage(imageId).then(image => {
+						this.image = image.src
+					})
+				}
+				return store.findTracks(channel.id).then(tracks => {
+					// Define an extra `active` prop. Otherwise Vue won't detect changes.
+					tracks.forEach(t => t.active = false)
+					this.tracks = tracks
+					return {channel, tracks}
+				})
+			})
+		},
+		afterModel() {
+		}
+	}
+}
+</script>
+
+<style scoped>
+.Radio4000Player {
+	display: flex;
+	flex-direction: column;
+	max-width: 320px;
+	height: 400px;
+	border: 1px solid hsl(0, 0%, 60%);
+	background-color: hsl(260, 10%, 92% );
+	color: hsl(0, 0%, 10%);
+	font-family: 'maisonneue', 'system-ui', sans-serif;
+	font-size: 1em;
+}
+header {}
+aside {}
+main {
+	flex: 1;
+	overflow-y: scroll;
+	overflow-x: hidden;
+}
+footer {}
+</style>
+
+<style id="Radio4000-mini">
+/* Mini skin. Hides track list and "play/pause" button */
+/*
+.Radio4000Player {height: auto !important;}
+.Radio4000Player main {display: none;}
+.Radio4000Player menu {border-top: 0;}
+.Radio4000Player menu button:first-child {display: none;}
+ */
+.Radio4000Player.mini {height: auto !important;}
+.Radio4000Player.mini main {display: none;}
+.Radio4000Player.mini menu {border-top: 0;}
+.Radio4000Player.mini menu button:first-child {display: none;}
+</style>
+
+<style id="Radio4000-dark">
+div.Radio4000Player.dark,
+div.Radio4000Player.dark button {
+	background-color: hsl(100, 30%, 65%);
+	background-color: hsl(0, 0%, 0%);
+	color: hsl(0, 0%, 80%);
+}
+div.Radio4000Player.dark {
+	border-color: hsl(0, 0%, 0%);
+}
+div.Radio4000Player.dark .TrackList li:before {
+	color: hsla(0, 0%, 100%, 0.5);
+}
+div.Radio4000Player.dark .Header-playing,
+div.Radio4000Player.dark .active {
+	color: hsl(0, 0%, 100%);
+}
+div.Radio4000Player.dark .R4 {
+	fill: hsla(0, 0%, 100%, 0.3);
+}
+/* Dark scrollbars for later. */
+/*
+::-webkit-scrollbar {
+background-color: hsla(0, 0%, 50%, 0.4);
+width: 0.75em;
+}
+::-webkit-scrollbar:hover {
+background-color: hsla(0, 0%, 50%, 0.6);
+}
+::-webkit-scrollbar-thumb {
+background-color: hsla(0, 0%, 50%, 0.4);
+}
+::-webkit-scrollbar-thumb:active {
+background: rgba(0,0,0,0.61); 
+}*/
+</style>
+
+<style id="Radio4000-purple">
+/* needs "dark" to be active as well
+div.Radio4000Player.dark,
+div.Radio4000Player.dark button {
+background-color: #5e1ae6;
+}*/
+</style>
--- src/main.js
+++ src/main.js
@@ -1,26 +1,9 @@
 import Vue from 'vue'
-import App from './App.vue'
+import vueCustomElement from 'vue-custom-element'
+import 'document-register-element/build/document-register-element'
+import Radio4000Player from './Radio4000Player.vue'
 
-// Pass attributes from the HTML element to the root Vue instance.
-function getAttr(attrs, key) {
-	return attrs[key] ? attrs[key].value : ''
-}
+Vue.use(vueCustomElement)
 
-function createPlayer(el) {
-	const attrs = el.attributes
-	return new Vue({
-		el, 
-		render(h) {
-			return h(App, {
-				props: {
-					slug: getAttr(attrs, 'slug'),
-					volume: Number(getAttr(attrs, 'volume'))
-				}
-			})
-		}
-	})
-}
-
-// Support multiple components on the same page.
-let players = document.querySelectorAll('radio4000-player')
-players.forEach(el => createPlayer(el))
+// Register <radio4000-player> element to our Vue component.
+Vue.customElement('radio4000-player', Radio4000Player)
