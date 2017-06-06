e1f17ba6e1eda3975edbcc332d6b6304d9cbde36
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-06-06 11:51:17 +0200

    add lodash debounce locate track

--- src/TrackList.vue
+++ src/TrackList.vue
@@ -24,10 +24,17 @@
 <script>
 	import TrackItem from './TrackItem.vue'
 	import Loading from './Loading.vue'
+	import debounce from 'lodash/debounce'
 	export default {
 		name: 'track-list',
 		props: ['tracks', 'track', 'currentTrackIndex'],
 		components: { TrackItem, Loading },
+		watch: {
+			tracks: debounce(
+				function() {
+					this.locateCurrentTrack();
+				}, 400)
+		},
 		methods: {
 			select(track) {
 				this.$emit('select', track)
