9886c384d43e462b18e7ca505792ae1dee3f0134
Oskar Rough <oskar@rough.dk>
2017-07-04 19:35:31 +0200

    Update tests

--- test/player-controls.js
+++ test/player-controls.js
@@ -6,17 +6,17 @@ test.cb('renders', t => {
 	const vm = new Vue(Component).$mount()
 	const tree = {$el: vm.$el.outerHTML}
 	const $ = selector => vm.$el.querySelectorAll(selector)
-	const playpauseBtn = $('.PlayerControl-group--large')[0].querySelector('button')
+	const playPauseInput= $('.PlayPause input')[0]
 
 	t.snapshot(tree)
 
 	t.is($('.Btn--mute').length, 1)
 	t.is($('.Btn--shuffle').length, 1)
 
-	t.is(playpauseBtn.textContent.trim(), 'Play')
+	t.is(playPauseInput.checked, false)
 	vm.$props.isPlaying = true
 	vm.$nextTick(() => {
-		t.is(playpauseBtn.textContent.trim(), 'Pause')
+		t.is(playPauseInput.checked, true)
 		t.end()
 	})
 })
