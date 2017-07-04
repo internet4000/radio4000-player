3bb209f8aa24ab5493d40fbb075dccb5dcbc3e61
Oskar Rough <oskar@rough.dk>
2017-07-04 19:35:22 +0200

    Add new play-pause button that animates between icons

--- src/PlayerControls.vue
+++ src/PlayerControls.vue
@@ -10,7 +10,6 @@
 				<span></span>
 			</button>
 		</div>
-
 		<div class="PlayerControl-group">
 			<button title="Shuffle on/off"
 				class="Btn Btn--shuffle"
@@ -20,20 +19,13 @@
 				<span>↝</span>
 			</button>
 		</div>
-
 		<div class="PlayerControl-group PlayerControl-group--large">
-			<button v-if="!isPlaying"
-				class="Btn"
-				:disabled="isDisabled"
-				@click="$emit('play')"
-				title="Play">Play</button>
-			<button v-else
-				class="Btn"
-				:disabled="isDisabled"
-				@click="$emit('pause')"
-				title="Pause">Pause</button>
+			<PlayPauseButton class="Btn"
+				:isPlaying="isPlaying"
+				@play="$emit('play')"
+				@pause="$emit('pause')"
+			></PlayPauseButton>
 		</div>
-
 		<div class="PlayerControl-group PlayerControl-group--large">
 			<button
 				:disabled="isDisabled"
@@ -41,13 +33,14 @@
 				class="Btn"
 				title="Next track">Next</button>
 		</div>
-
 	</menu>
 </template>
 
 <script>
+	import PlayPauseButton from './PlayPauseButton.vue'
 	export default {
 		name: 'player-controls',
+		components: {PlayPauseButton},
 		props: [
 			'volume',
 			'isPlaying',
@@ -97,5 +90,9 @@
 	}
 	.Btn--shuffle.is-active span {
 		opacity: 0.5;
+	}
+	.PlayPause {
+		padding-top: 0;
+		padding-bottom: 0;
 	}
 </style>
