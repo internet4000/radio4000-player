e1957e710da4881989ed6726d83933673b134cc6
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-07-15 12:54:56 +0200

    add previousTrack to trackChanged event

--- src/Radio4000Player.vue
+++ src/Radio4000Player.vue
@@ -115,8 +115,12 @@
 		methods: {
 			playTrack(track) {
 				console.log('playTrack')
+				const previousTrack = this.currentTrack;
 				this.currentTrack = track
-				this.$emit('trackChanged', track)
+				this.$emit('trackChanged', {
+					track,
+					previousTrack
+				})
 			},
 			newTracksPool() {
 				var newTracksPool = this.tracks.slice().reverse()
