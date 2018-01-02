9333d421de556b3d4ed25f04a7443dfb214c5ea5
Oskar Rough <oskar@rough.dk>
2018-01-02 23:24:42 +0100

    Add tests
    Can be run with `yarn cypress open` and then pressing the button. Or just `yarn cypress run`.

---
+++ cypress/integration/shuffle.js
@@ -0,0 +1,15 @@
+describe('shuffle', function() {
+	before(() => {
+		cy.visit('http://localhost:4002')
+	})
+
+	it('pressing "shuffle" shuffles the track list', function() {
+		cy.get('.TrackItem-title').eq(1).then($el => {
+			var title = $el.text()
+			cy.get('.Btn--shuffle').click()
+			cy.get('.TrackItem-title').eq(1).should('not.contain', title)
+			cy.get('.Btn--shuffle').click()
+			cy.get('.TrackItem-title').eq(1).should('contain', title)
+		})
+	})
+})
