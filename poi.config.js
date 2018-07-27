65f3b8f64cc1e2d377adfc34060bd07998bf4d89
Oskar Rough <oskar@rough.dk>
2018-07-27 17:41:22 +0200

    Fix build issue
    - with new version of media-url-parser
    - specify that we don't want node's `url` in our bundle

---
+++ poi.config.js
@@ -0,0 +1,28 @@
+// poi.config.js
+module.exports = {
+	port: 4002,
+	html: {
+		template: 'index.html'
+	},
+	extractCSS: false,
+	filename: {
+		js: 'radio4000-player.min.js'
+	},
+
+	// Don't split bundle in two files
+	vendor: false,
+
+	// Package as UMD module.
+	format: 'umd',
+	moduleName: 'radio4000Player',
+
+	transformModules: ['media-url-parser'],
+
+	webpack(config) {
+		// Exclude `url` module and rely on global in browsers.
+		config.externals = {
+			url: 'URL'
+		}
+		return config
+	}
+}
