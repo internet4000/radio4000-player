353921a826360a331698210c46b0f1593784dfb7
Oskar <oskar@rough.dk>
2018-10-17 20:41:28 +0200

    Use same image size as on radio4000.com

--- src/utils/store.js
+++ src/utils/store.js
@@ -51,6 +51,6 @@ export function findTrack(id) {
 }
 
 export function buildThumbnail(cloudinaryId) {
-	return `https://res.cloudinary.com/radio4000/image/upload/w_120,h_120,q_60,c_thumb,fl_lossy/${cloudinaryId}`
+	return `https://res.cloudinary.com/radio4000/image/upload/w_250,h_250,q_60,c_thumb,fl_lossy/${cloudinaryId}`
 }
 
