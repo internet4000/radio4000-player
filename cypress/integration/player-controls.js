e7b7cc0310c5d3b0f7a9611cfe543230c3577b2f
uHGRP <hugurp@protonmail.com>
2019-11-29 15:31:53 +0100

    fix tests broken by window not fully loaded

--- cypress/integration/player-controls.js
+++ cypress/integration/player-controls.js
@@ -15,25 +15,33 @@ describe('player controls', function() {
 	})
 
 	it('first track is selected', function() {
-		cy.get('radio4000-player').find('.TrackItem').first().should('have.class', 'active')
+		cy.on('window:load', function() {
+			cy.get('radio4000-player').find('.TrackItem').first().should('have.class', 'active')
+		})
 	})
 
 	it('tapping "next" does not play next track', function() {
-		cy.get('.Btn--next').click()
-		cy.get('.TrackItem').eq(0).should('not.have.class', 'active')
-		cy.get('.TrackItem').eq(1).should('have.class', 'active')
-		cy.get('.PlayPause-state', {timeout: 20000}).should('not.be.checked')
+		cy.on('window:load', function() {
+			cy.get('.Btn--next').click()
+			cy.get('.TrackItem').eq(0).should('not.have.class', 'active')
+			cy.get('.TrackItem').eq(1).should('have.class', 'active')
+			cy.get('.PlayPause-state', {timeout: 20000}).should('not.be.checked')
+		})
 	})
 
 	it('tapping "next" plays next track when isPlaying', function() {
-		cy.get('.PlayPause-label').click()
-		cy.get('.Btn--next', {timeout: 20000}).click()
-		cy.get('.PlayPause-state', {timeout: 20000}).should('be.checked')
+		cy.on('window:load', function() {	
+			cy.get('.PlayPause-label').click()
+			cy.get('.Btn--next', {timeout: 20000}).click()
+			cy.get('.PlayPause-state', {timeout: 20000}).should('be.checked')
+		})
 	})
 
 	it('tapping "pause" pauses', function() {
-		cy.get('.PlayPause-state').should('be.checked')
-		cy.get('.PlayPause-label').click()
-		cy.get('.PlayPause-state').should('not.be.checked')
+		cy.on('window:load', function() {
+			cy.get('.PlayPause-state').should('be.checked')
+			cy.get('.PlayPause-label').click()
+			cy.get('.PlayPause-state').should('not.be.checked')
+		})
 	})
 })
